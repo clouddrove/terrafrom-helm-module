78038d0278f4ae2989e7e3e9d4e7c9185806918e
anmolnagpal <anmol.nagpal@deepimmo.com>
2022-11-04 23:09:26 +0530

    fixed readme.yaml

--- _example/example.tf
+++ _example/example.tf
@@ -1,13 +1,13 @@
 module "helm_deploy" {
-  source          = "../"
-  name            = "metrics-server"
-  repository      = "https://charts.bitnami.com/bitnami"
-  chart           = "metrics-server"
-  chart_version   = "5.0.1"
-  namespace       = "kube-system"
-  config_path     = "./kubeconfig/kube_cluster.yaml"
+  source        = "../"
+  name          = "metrics-server"
+  repository    = "https://charts.bitnami.com/bitnami"
+  chart         = "metrics-server"
+  chart_version = "5.0.1"
+  namespace     = "kube-system"
+  config_path   = "./kubeconfig/kube_cluster.yaml"
 
-  values         = [
+  values = [
     <<-EOF
     fullnameOverride: metrics-server
     EOF
